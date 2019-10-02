59e18d1e2b32e863143e557f85dbc942958062ff
Guillermo <guillhermoherrera@gmail.com>
2019-10-02 14:08:38 -0500

    imagepicker

--- lib/pages/solicitud2.dart
+++ lib/pages/solicitud2.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sgcartera_app/models/solicitud.dart';
 
 class SolicitudArchivos extends StatefulWidget {
@@ -11,6 +14,9 @@ class SolicitudArchivos extends StatefulWidget {
 }
 
 class _SolicitudArchivosState extends State<SolicitudArchivos> {
+  File identidicacionFile;
+  File domicilioFile;
+  File buroFile;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,8 +65,9 @@ class _SolicitudArchivosState extends State<SolicitudArchivos> {
         ),
       ),
       Divider(),
-      datosPrevios(),
+      adjuntarId(),
       Divider(),
+      datosPrevios(),
       Column(
         children: buttonWidget(),
       ),
@@ -71,6 +78,115 @@ class _SolicitudArchivosState extends State<SolicitudArchivos> {
         ],
       )
     ];
+  }
+
+  Widget adjuntarId(){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        itemsFiles("IDENTIFICACIÓN", 1),
+        Divider(),
+        itemsFiles("COMPROBANTE\n DE DOMICILIO", 2),
+        Divider(),
+        itemsFiles("AUTORIZACIÓN\n DE BURÓ", 3),
+      ],
+    );
+  }
+
+  Widget itemsFiles(titulo, tipo){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center ,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(left: 10, right: 10),
+          child: Text(titulo),
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 10, right: 10),
+          child: Column(children: <Widget>[
+            ButtonTheme(
+              minWidth: 50.0,
+              height: 30.0,
+              child:RaisedButton(onPressed: ()=> imageSelectorGallery(1,tipo), child: Icon(Icons.add_photo_alternate),color: Colors.blue,textColor: Colors.white,)
+            ),
+            ButtonTheme(
+              minWidth: 50.0,
+              height: 30.0,
+              child:RaisedButton(onPressed: ()=> imageSelectorGallery(2,tipo), child: Icon(Icons.add_a_photo),color: Colors.blue,textColor: Colors.white)
+            ),
+          ],) 
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 10, right: 10),
+          child: SizedBox(
+            child: showImage(tipo),
+            width: 100,
+            height: 100,
+          ),
+        )
+      ],
+    );
+  }
+
+  imageSelectorGallery(opc, tipo) async{
+    File auxFile;
+    try{
+      if(opc == 1)
+        auxFile = await ImagePicker.pickImage(
+          source: ImageSource.gallery,
+          maxHeight: 800.0,
+          maxWidth: 700.0
+        );
+      else{
+        auxFile = await ImagePicker.pickImage(
+          source: ImageSource.camera,
+          maxHeight: 800.0,
+          maxWidth: 700.0
+        );
+      }
+    }catch(e){
+
+    }
+    switch (tipo){
+      case 1:
+        identidicacionFile = auxFile;
+        break;
+      case 2:
+        domicilioFile = auxFile;
+        break;
+      case 3:
+        buroFile = auxFile;
+        break;
+    }
+    setState(() {});
+  }
+
+  showImage(tipo){
+    File auxFile;
+    switch (tipo){
+      case 1:
+        auxFile = identidicacionFile;
+        break;
+      case 2:
+        auxFile = domicilioFile;
+        break;
+      case 3:
+        auxFile = buroFile;
+        break;
+    }
+    if(auxFile != null)
+      //return Image.file(auxFile);
+      return Hero(
+        tag: "image"+tipo.toString(),
+        child: GestureDetector(
+          onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (context)=> ImageDetail(tipo: tipo,image: auxFile))),
+          child: Image.file(auxFile)
+        ),
+      );
+    else
+      return Image.asset("images/noImage.png");
   }
 
   Widget datosPrevios(){
@@ -126,6 +242,29 @@ class _SolicitudArchivosState extends State<SolicitudArchivos> {
       color: Colors.blue,
       textColor: Colors.white,
       child: Text(text),
+    );
+  }
+}
+
+class ImageDetail extends StatelessWidget {
+  ImageDetail({this.tipo, this.image});
+  final int tipo;
+  final File image;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Text(tipo == 1 ? "IDENTIFICACÓN" : tipo == 2 ? "COMPROBANTE DE DOMICILIO" : "AUTORIZACIÓN DE BURO")
+      ),
+      body: Center(
+        child: Hero(
+          tag: "image"+tipo.toString(),
+          child: Image.file(image)
+        )
+      ),
     );
   }
 }
